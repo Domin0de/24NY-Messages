9b6f1e5a7290b26e23423c5edcad5f0e0b5a81b0
Wolfdragon24 <dragonbomber24@gmail.com>
2024-12-13 23:15:04 +1100

    Implementation of hook component and player fixes for camera movement

--- player.gd
+++ player.gd
@@ -1,39 +1,46 @@
 extends CharacterBody3D
 
-@export var speed = 8
-@export var gravity_acceleration = 20
-@export var jump_speed = 8
+@export var speed = 10
+@export var gravity = 20
+@export var jump_speed = 10
+@export var sensitivity = 500
 
-var target_velocity = Vector3.ZERO
+var cam_move = false
+@onready var spring_arm = $SpringArm3D
 
-func _physics_process(delta: float) -> void:
-	var direction = Vector3.ZERO
+func _physics_process(delta):
+	get_move_input(delta)
 
-	# Set direction relative to base axes relative to inputs
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-
+	move_and_slide()
+	
 	if Input.is_action_pressed("jump"):
-		target_velocity.y = jump_speed
+		velocity.y = jump_speed
 
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		$Pivot.basis = Basis.looking_at(direction)
-
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+	if Input.is_action_pressed("aim"):
+		cam_move = true
+		
+		
+	else:
+		cam_move = false
 
 	if not is_on_floor():
-		target_velocity.y -= (gravity_acceleration * delta * 1/2)
+		velocity.y -= (gravity * delta * 1/2)
 
-	velocity = target_velocity
-	move_and_slide()
+func get_move_input(delta):
+	var stored_y = velocity.y
+	velocity.y = 0
 
-	# Match position and facing of camera follower with self
-	$Camera_Follower.position = lerp($Camera_Follower.position, position, 0.1)
+	var input = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = Vector3(input.x, 0, input.y).rotated(Vector3.UP, rotation.y)
+
+	velocity = lerp(velocity, direction * speed, 0.5)
+	velocity.y = stored_y
+
+func _input(event):
+	if event is InputEventMouseMotion and cam_move:
+		rotation.x -= event.relative.y / sensitivity
+		rotation_degrees.x = clamp(rotation_degrees.x, -90.00, 30)
+		rotation.y -= event.relative.x / sensitivity
+		
+	if event.is_action_pressed("shoot"):
+		pass
